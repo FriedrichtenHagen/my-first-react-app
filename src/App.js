bb5b37489313bbfac860d2c4ea4eee8d1328f80a
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-04-01 21:34:20 +0200

    add a dynamically created list from an array of objects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
+  const listOfProducts = products.map(product => 
+      <li key={product.id}>
+      {product.title}
+    </li>
+  )
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +22,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          <ul>{listOfProducts}</ul>
+        
       </header>
     </div>
   );
